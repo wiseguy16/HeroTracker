5bf96ade9c44639e16d32b143e1012b6c54d46f6
wiseguy16 <gergusa04@gmail.com>
2016-07-25 11:52:32 -0400

    Initial Commit

---
+++ HeroTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HeroTracker
+//
+//  Created by Gregory Weiss on 7/25/16.
+//  Copyright © 2016 Gregory Weiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HeroTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HeroTracker
+//
+//  Created by Gregory Weiss on 7/25/16.
+//  Copyright © 2016 Gregory Weiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
